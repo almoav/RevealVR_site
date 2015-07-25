43f4d17e450a413223aa3bca13f5825e6f34a802
Alex <almoav@gmail.com>
2015-07-24 23:31:53 -0700

    add jquery content navigation

---
+++ js/viewModel.js
@@ -0,0 +1,46 @@
+// view model script controls swapping of mid wrapper content
+
+
+$(".nav-home").click(function() {
+	showSection($(".home"));
+});
+
+
+$(".nav-work").click(function() {
+	showSection($(".work"));
+});
+
+$(".nav-about").click(function() {
+	showSection($(".about"));
+});
+
+$(".nav-faq").click(function() {
+	showSection($(".faq"));
+});
+
+$(".nav-contact").click(function() {
+	showSection($(".contact"));
+});
+
+$(".nav-blog").click(function() {
+	showSection($(".blog"));
+});
+
+var showSection = function(element) {
+	console.log(this);
+	$(".content").toggleClass("hidden", true);
+	element.toggleClass("hidden", false);
+};
+
+Content = function(data) {
+	// stores the inner html for the different mid content containers
+	var self = this;
+
+}
+
+
+ViewModel = function() {
+	var self = this;
+};
+
+ko.applyBindings(new ViewModel());
